77a41eb187481d9e754ad452997bd3fc2f3000c3
Admin <bermetsaadat@gmail.com>
2022-07-30 20:24:13 +0600

    commit3

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("hi");
+        System.out.println("hgdhg");
+    }
+}
